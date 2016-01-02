d3957672d3bf686be001c7aa1e6ec88d343d4afa
shoumu <shoumuzyq@gmail.com>
2016-01-02 23:58:51 +0800

    solve leetcode problem 309

--- leetcode/309_best_time_buy_sell_stock_cooldown.py
+++ leetcode/309_best_time_buy_sell_stock_cooldown.py
@@ -6,3 +6,22 @@
 
 
 def max_profit(prices):
+    if len(prices) < 2:
+        return 0
+    last = dlast = 0
+    for i in range(1, len(prices)):
+        temp = last
+        if last + prices[i] - prices[i - 1] > dlast:
+            last = last + prices[i] - prices[i - 1]
+        else:
+            last = dlast
+        if temp > dlast:
+            dlast = temp
+    if dlast > last:
+        return dlast
+    else:
+        return last
+
+
+prices = [1, 2, 3, 0, 2]
+print(max_profit(prices))
