d7bc0acff3fa7487bd8d288d23b4c11dba9bfbb9
shoumu <shoumuzyq@gmail.com>
2016-02-24 23:56:24 +0800

    solve leetcode problem 312

--- leetcode/312_burst_balloons.py
+++ leetcode/312_burst_balloons.py
@@ -6,4 +6,21 @@
 
 
 def max_coins(nums):
-    
+    nums = [1] + nums + [1]
+    n = len(nums)
+    dp = [[0] * n for i in range(n)]
+
+    def calculate(i, j):
+        if dp[i][j] or i == j - 1:
+            return dp[i][j]
+        coins = 0
+        for k in range(i + 1, j):
+            coins = max(coins, nums[i] * nums[k] * nums[j] + calculate(i, k) + calculate(k, j))
+        dp[i][j] = coins
+        return coins
+
+    return calculate(0, n - 1)
+
+
+nums = [3, 1, 5, 8]
+print(max_coins(nums))
